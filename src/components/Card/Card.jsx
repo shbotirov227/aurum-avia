efc0383bc24aa3ddb3b70f502695dd0d97a37050
Shohruh Botirov <shbotirov227@gmail.com>
2024-07-14 17:42:45 +0500

    start

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./Card.scss";
+
+const Card = () => {
+    return (
+        <div className="Card">
+            <h2>Card</h2>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/components/ContactCard/ContactCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./ContactCard.scss"
+
+const ContactCard = () => {
+    return (
+        <div className="ContactCard">
+            <h2>ContactCard</h2>
+        </div>
+    )
+}
+
+export default ContactCard;
