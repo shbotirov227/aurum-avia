0a2e1d5161bd7fe33ef24339dd1a9529de7a50fa
Shohruh Botirov <shbotirov227@gmail.com>
2024-07-17 16:35:55 +0500

    redesigned

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
 	return (
 		<div className="Header" id="header">
 			<div className="Header-inner container">
-				<a href="/"><img src={Logo} alt="logo" /></a>
+				<a href="/"><img className="logo" src={Logo} alt="logo" /></a>
 				<ul className="Header-links">
 					<li><a className="Header-link" href="/">{t("header.home")}</a></li>
 					<li><a className="Header-link" href="#about">{t("header.about")}</a></li>
