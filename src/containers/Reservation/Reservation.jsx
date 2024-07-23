babf1f6f1f4b5c3f20a1c34d9e9863504bb666e7
abikIbragim <abikibragim@icloud.com>
2024-07-23 19:11:12 +0500

    ressss

--- src/containers/Reservation/Reservation.jsx
+++ src/containers/Reservation/Reservation.jsx
@@ -6,6 +6,10 @@ import "./Reservation.scss";
 const Reservation = () => {
     return (
         <div className="Reservation" id="contact">
+            <h4 className="Reservation-text">Book Prefered Deal Here</h4>
+            <h3 className="Reservation-title">Make Your Reservation</h3>
+            <a href="#contact" className="Reservation-btn">Discover More</a>
+
             <Contact />
         </div>
     )
