34fc2924a76400df560c73066f2f896f292c9c92
shbotirov227 <shbotirov227@gmail.com>
2025-05-14 03:06:49 +0500

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./pages/Home/Home";
 import { Analytics } from "@vercel/analytics/react"
-import EzmobAds from "./components/EzmobAds";
+// import EzmobAds from "./components/EzmobAds";
 
 import "./assets/index.scss";
 
@@ -10,7 +10,7 @@ function App() {
 		<div className="App">
 			<Home />
             <Analytics />
-            <EzmobAds />
+            {/* <EzmobAds /> */}
 		</div>
 	);
 }
